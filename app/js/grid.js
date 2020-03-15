80c9b5238c42e373bd358ca696e77a0af21500e8
danilo.albudane <daniloassis_vg@hotmail.com>
2020-03-15 12:55:20 -0300

    update: filter

--- app/js/grid.js
+++ app/js/grid.js
@@ -3,6 +3,11 @@ import mockNoticias from './mock/mockNoticias';
 const elNewsContainer = document.querySelector('.grid__list')
 ;
 
+var filtro=document.querySelector('#editoriais');
+filtro.addEventListener("click",load());
+
+let htmlGrid = '';
+
 function load(){
 
   const valorDoSelect = document.querySelector('#editoriais').value;
@@ -11,14 +16,30 @@ function load(){
   edFil.forEach(edFil => {
 
     if ((valorDoSelect === '') || (edFil.Editoria === valorDoSelect)) {
-      htmlTeste += `
-      `;
-    }
+      edFil['Notícias'].forEach(noticia => {
+        htmlGrid += `
+        <div class='noticia'>
+                <div>
+                  <p>${editoria.Editoria}</p>
+                  <p>${noticia["Data de publicação"]}</p>
+                </div>
+                <img src="../img/${noticia.Foto}" alt="${noticia.Titulo}" />
+                <div class='conteudo'>
+                  <p>${noticia.Título}</p>
+                  <p>${noticia.Texto}</p>
+                  <br><br>
+                  <p><b>Saiba Mais</b></p>
+                </div>
+          </div>
+        `;
+      });
+      }
   });
+
+  elNewsContainer.innerHTML = htmlGrid;
 }
 
-var filtro=document.querySelector('#editoriais');
-filtro.addEventListener("click",load);
+
 
   // window.addEventListener("load",load);
 
@@ -57,3 +78,4 @@ const changeNews = () => {
 };
 
 changeNews();
+load();
